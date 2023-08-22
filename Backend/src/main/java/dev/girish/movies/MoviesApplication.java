95675cb52e16ff4d7633b384cd52e0ba6fc089fd
Girish <girish.170902@gmail.com>
2023-08-22 23:07:03 +0530

    Completed Front End for the application

--- Backend/src/main/java/dev/girish/movies/MoviesApplication.java
+++ Backend/src/main/java/dev/girish/movies/MoviesApplication.java
@@ -2,11 +2,16 @@ package dev.girish.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @RestController
+@CrossOrigin
 public class MoviesApplication {
 
 	public static void main(String[] args) {
@@ -14,3 +19,4 @@ public class MoviesApplication {
 	}
 
 }
+
